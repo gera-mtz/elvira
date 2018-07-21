78431904fced2e962b6d97c0e48c31ba09da68e6
Jose Gerardo Martinez Moran <geramtz100@gmail.com>
2018-07-21 00:23:02 -0500

    collapse listo

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,11 @@ $('.carousel').carousel({
     pause: "false" /* Change to true to make it paused when your mouse cursor enter the background */
 });
 
+$('[data-toggle="slide-collapse"]').on('click', function() {
+    $navMenuCont = $($(this).data('target'));
+    $navMenuCont.animate({'width':'toggle'}, 350);
+});
+
 $(window).scroll(function () {
 	if ($(window).scrollTop() >= 50) {
 		$('nav').css('background','#000');
