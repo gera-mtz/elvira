a4dae21b1dd7fd14c1d712cd82e3384594853a02
Jose Gerardo Martinez Moran <geramtz100@gmail.com>
2018-07-21 03:02:21 -0500

    menu responsivo

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,27 @@ $('.carousel').carousel({
     pause: "false" /* Change to true to make it paused when your mouse cursor enter the background */
 });
 
-$('[data-toggle="slide-collapse"]').on('click', function() {
-    $navMenuCont = $($(this).data('target'));
-    $navMenuCont.animate({'width':'toggle'}, 350);
-});
 
 $(window).scroll(function () {
 	if ($(window).scrollTop() >= 50) {
 		$('nav').css('background','#000');
 	} else {
 		$('nav').css('background','transparent');
+	}
+});
+
+$(window).resize(function(){     
+	if ($('#buttonResponsive').css('display') != 'none'){
+		$('#navbarSidebar').css('display', 'none');
+	} else{
+		$('#navbarSidebar').css('display', 'block');
+	}
+});
+
+$('#buttonResponsive').on('click', function(){
+	if ($('#navbarSidebar').css('display') != 'none'){
+		$('#navbarSidebar').css('display', 'none');
+	} else{
+		$('#navbarSidebar').css('display', 'block');
 	}
 });
