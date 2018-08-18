c6a32ae04d2afe20ee886cdaf7e36bea04be4513
Jose Gerardo Martinez Moran <geramtz100@gmail.com>
2018-08-18 16:35:00 -0500

    actor responsivo desde 568px para arriba

--- js/slick-custom.js
+++ js/slick-custom.js
@@ -8,8 +8,41 @@ $('.slider-for').slick({
 
 $('.slider-nav').slick({
 	slidesToShow: 6,
-	slidesToScroll: 6,
+	slidesToScroll: 1,
 	asNavFor: '.slider-for',
-	dots: true,
-	focusOnSelect: true
+	dots: false,
+	infinity: false,
+	focusOnSelect: true,
+	responsive: [
+		{
+			breakpoint: 1086,
+			settings:{
+				slidesToShow: 5,
+				slidesToScroll: 1,
+				dots: false,
+				focusOnSelect: true,
+				infinity: false
+			}
+		},
+		{
+			breakpoint: 992,
+			settings:{
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				dots: false,
+				focusOnSelect: true,
+				infinity: false
+			}
+		},
+		{
+			breakpoint: 768,
+			settings:{
+				slidesToShow: 5,
+				slidesToScroll: 1,
+				dots: false,
+				focusOnSelect: true,
+				infinity: false
+			}
+		}
+	]
 });
